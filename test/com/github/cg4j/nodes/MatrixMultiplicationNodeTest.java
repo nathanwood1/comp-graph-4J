e9cfb0b0b730562f323cae0316e36d8e8eb71ba7
nathan <nathanforlifeforever@gmail.com>
2017-07-31 15:19:11 +0100

    Added 1 more test for 'MatrixMultiplicationModeTest' just to be 100% sure the algorithm works.

--- test/com/github/cg4j/nodes/MatrixMultiplicationNodeTest.java
+++ test/com/github/cg4j/nodes/MatrixMultiplicationNodeTest.java
@@ -82,4 +82,36 @@ class MatrixMultiplicationNodeTest {
         }, new int[]{2, 2, 2, 2}).equals(eval.evaluate(c)));
     }
 
+    @Test
+    void evaluate3() {
+        InputNode a = new InputNode(new int[]{-1, 2, 3});
+        InputNode b = new InputNode(new int[]{2, 3, 2});
+        MatrixMultiplicationNode c = new MatrixMultiplicationNode(a, b);
+
+        Tensor aInput = new Tensor(new float[]{
+                1, 2, 3,
+                4, 5, 6,
+        }, new int[]{1, 2, 3});
+        Tensor bInput = new Tensor(new float[]{
+                7, 8,
+                9, 10,
+                11, 12,
+
+                13, 14,
+                15, 16,
+                17, 18
+        }, new int[]{2, 3, 2});
+
+        Eval eval = new Eval()
+                .addInput(a, aInput)
+                .addInput(b, bInput);
+        Assertions.assertTrue(new Tensor(new float[]{
+                58, 64,
+                94, 100,
+
+                139, 154,
+                229, 244,
+        }, new int[]{1, 2, 2, 2}).equals(eval.evaluate(c)));
+    }
+
 }
